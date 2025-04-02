ed7a3194f8d413cf023c06b7141554f40ee1c673
Ignyra <ahmedsamir09000@gmail.com>
2025-04-03 01:25:21 +0300

    faster startup time with filetype checking for lspconfig

--- lua/configs/languages.lua
+++ lua/configs/languages.lua
@@ -1,46 +1,36 @@
 
 local languages_config = {
-  -- Format: { treesitter_name, mason_name, lsp_settings }
+  -- Format: { treesitter_name, mason_name, filetypes }
   -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-  { "lua", "lua_ls", {
-      settings = {
-        Lua = { diagnostics = { globals = { "vim" } } }
-      }
-    }
-  },
-  { "python", "pyright", {} },
-  { "javascript", "ts_ls", {} },
-  { "typescript", "ts_ls", {} },
-  { "html", "html", {} },
-  { "css", "cssls", {} },
-  { "rust", "rust_analyzer", {} },
-  {"asm", "asm_lsp", {require "plugins.other.asm",-- Needs external insallation of cargo
-    }
-  },
+  { "lua", "lua_ls", {"lua"}},
+  { "python", "pyright", {"python"} },
+  { "javascript", "ts_ls", {"javascript"} },
+  { "typescript", "ts_ls", {"typescript"} },
+  { "html", "html", {"html"} },
+  { "css", "cssls", {"css"} },
+  { "rust", "rust_analyzer", {"rust"} },
+  {"asm", "asm_lsp", {"asm"}},--require "plugins.other.asm",-- Needs external insallation of cargo},
   { "scala", "", {}}
 }
-
+local servers = {}
 local languages = {}
+local filetype_lookup = {}
 for _, lang in ipairs(languages_config) do
   table.insert(languages, lang[1])
-end
-
-local servers = {}
-for _, lang in ipairs(languages_config) do
   if lang[2] ~= "" then
     table.insert(servers, lang[2])  -- Only add Mason-compatible entries
+    for _, file in ipairs(lang[3]) do
+      filetype_lookup[file] = lang[2]
+    end
   end
 end
 
-local opts_lookup = {}
-for _, lang in ipairs(languages_config) do
-  if lang[2] ~= "" then -- Use Mason name if available
-    opts_lookup[lang[2]] = lang[3]
-  end
-end
+
+
 
 return {
   languages,
   servers,
-  opts_lookup
+  filetype_lookup
+  
 }
