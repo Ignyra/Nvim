340c7ce92870960443fb4cfca9505a238a7e911e
Ignyra <ahmedsamir09000@gmail.com>
2025-04-03 19:36:21 +0300

    neovim installation

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,18 +1,15 @@
 --Automatic Starting is triggered only with specifc events like FileType and BufReadPost but the first takes a long time
 --while the second doesn't have the filetype loaded yet.
 vim.api.nvim_create_autocmd("UIEnter", {
-    pattern = "*",
+    --pattern = "*",
     callback = function()
-        vim.cmd("LspStart")
+      if vim.bo.filetype == "markdown" then
+        -- Trigger Markdown
+        vim.api.nvim_exec_autocmds("User", { pattern = "MarkdownLoad" })
+      end
+   
+      vim.cmd("LspStart")
     end,
 })
 
 
-vim.api.nvim_create_autocmd("UIEnter", {
-    callback = function()
-        if vim.bo.filetype == "markdown" then
-            -- Trigger Markdown
-            vim.api.nvim_exec_autocmds("User", { pattern = "MarkdownLoad" })
-        end
-    end,
-})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,7 @@
 --local servers = require("configs.languages")[2]
 local filetype_lookup = require("configs.languages")[3]
 local lspconfig = require("lspconfig")
+local mason_lspconfig = require("mason-lspconfig")
 local M = require("configs.lspconfig")
 
 --Checking every server and loading lspconfig takes extra around 7 extra ms than checking one specifc server
@@ -25,7 +26,7 @@ local M = require("configs.lspconfig")
 --  lspconfig[server].setup(opts_lookup[server])
 --end
 
-require("mason-lspconfig").setup({
+mason_lspconfig.setup({
   automatic_installation = true
 })
 
@@ -33,7 +34,14 @@ require("mason-lspconfig").setup({
 local filetype = vim.bo.filetype
 local server = filetype_lookup[filetype]
 
+
+
 if server ~= nil then
+  --local mason_server = mason_lspconfig.get_mappings()["lspconfig_to_mason"][server]
+  --if not require("mason-registry").is_installed(mason_server) then 
+  --  require("mason.api.command").MasonInstall(mason_server)
+  --end
+  
   lspconfig[server].setup({
     on_attach = M.on_attach,
     on_init = M.on_init,
