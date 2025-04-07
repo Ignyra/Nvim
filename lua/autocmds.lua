e6b7e78b567645556f8b880bf49c6a335acef682
Ignyra <182546542+Ignyra@users.noreply.github.com>
2025-04-07 21:25:53 +0300

    New buffers and files will trigger lsp initialization for them too

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,38 +1,36 @@
 --Automatic Starting is triggered only with specifc events like FileType and BufReadPost but the first takes a long time
 --while the second doesn't have the filetype loaded yet.
-vim.api.nvim_create_autocmd("UIEnter", {
-    --pattern = "*",
-    callback = function()
-      if vim.bo.filetype == "markdown" then
-        -- Trigger Markdown
-        vim.api.nvim_exec_autocmds("User", { pattern = "MarkdownLoad" })
-      end
 
-      vim.schedule(function ()
-        if vim.g.lsp_server ~= nil then
-          vim.cmd("LspStart " .. vim.g.lsp_server)
-        end
-      end)
 
-      --Install lsp server if not attached by certain time
-      vim.defer_fn(function()
-        if vim.g.lsp_state ~= "Lsp Attached" and vim.g.lsp_server ~= nil then
-          if vim.fn.filereadable(vim.api.nvim_buf_get_name(0)) == 0 then
-            vim.notify("Save your new file, and reopen it to attach a server")
-            return
-          end
-          require("mason-lspconfig").setup()
-          vim.cmd("LspInstall " .. vim.g.lsp_server)
-          vim.notify("[INFO] " .. vim.g.lsp_server .. " took too long to load\n" .. "[INFO] Restart nvim after this installation attempt of the server")
-        end
-      end, 4000)
-    end,
+
+vim.api.nvim_create_autocmd({'BufReadPost', 'BufNewFile'}, {
+  callback = function(args)
+    vim.schedule(function()
+      vim.g.lspattach_on_filedetection() --wait for the function to be created by lsp.lua
+    end)
+    
+    --vim.api.nvim_create_autocmd("BufWinEnter", {
+    --  buffer = args.buf,
+    --  once = true,
+    --  callback = vim.g.lspattach_on_filedetection
+    --})
+  end
 })
+
+
 
 --Mark as attached so to not waste time on rensuring the server installation
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function ()
-    vim.g.lsp_state = "Lsp Attached"
+  callback = function (args)
+    local bufnr = args.buf
+    vim.g.attached_lsps = vim.tbl_deep_extend(
+    "force",
+    vim.g.attached_lsps,
+    {[vim.bo[bufnr].filetype] = "Lsp Attached"}
+    )
+
+    --vim.api.nvim_buf_set_var(bufnr, "lsp_state", "Lsp Attached") 
+    --vim.g.lsp_state = "Lsp Attached"
   end
 })
 
